06586dbf44e4432649b31ee88d5b837cfaa80edd
develop-joseavila <josecarlos.avilaperea@gmail.com>
2023-11-08 07:43:02 -0500

    feat: add messages area

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -10,11 +10,36 @@ class ChatScreen extends StatelessWidget {
         leading: const Padding(
           padding: EdgeInsets.all(4),
           child: CircleAvatar(
-            backgroundImage: NetworkImage('https://i.pinimg.com/474x/e3/bf/45/e3bf454c13590ec9bb6c093a4c01f45a.jpg'),
+            backgroundImage: NetworkImage(
+                'https://i.pinimg.com/474x/e3/bf/45/e3bf454c13590ec9bb6c093a4c01f45a.jpg'),
           ),
         ),
         title: const Text('Argemiro 😀'),
         centerTitle: true,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Text('$index');
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
