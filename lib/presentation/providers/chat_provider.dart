02791622191d65e832bbba2793928e7ccad157a0
develop-joseavila <josecarlos.avilaperea@gmail.com>
2023-11-09 08:09:15 -0500

    feat: add new message and update list name

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_maybe_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> message = [
+  List<Message> messagesList = [
     Message(text: 'Hablame!', fromWho: FromWho.me),
     Message(text: 'Vamos a rodar 🏍?', fromWho: FromWho.me),
+    Message(text: 'Si', fromWho: FromWho.hers),
   ];
 
   Future<void> sendMessage(String text) async {}
