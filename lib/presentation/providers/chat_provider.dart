1f392b32af8b196656768f1572ca03efd431195d
develop-joseavila <josecarlos.avilaperea@gmail.com>
2023-11-10 07:11:07 -0500

    feat: update to conect the chatProvider which will move on the scroll down when a message be sent

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_maybe_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+
   List<Message> messagesList = [
     Message(text: 'Hablame!', fromWho: FromWho.me),
     Message(text: 'Vamos a rodar 🏍?', fromWho: FromWho.me),
@@ -9,8 +11,19 @@ class ChatProvider extends ChangeNotifier {
   ];
 
   Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
     final Message newMessage = Message(text: text, fromWho: FromWho.me);
     messagesList.add(newMessage);
     notifyListeners();
+    modeScrollToBottom();
+  }
+
+  Future<void> modeScrollToBottom() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    chatScrollController.animateTo(
+      chatScrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeOut,
+    );
   }
 }
