9c3e0bd8f7e4254a956b1c6c3b0ff249f08d8a44
develop-joseavila <josecarlos.avilaperea@gmail.com>
2023-11-10 07:52:18 -0500

    feat: messagesList cleaned, added instructions to fetch api, fetch api, added her reply

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,24 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_maybe_app/config/helpers/get_yes_no_answer.dart';
 import 'package:flutter_maybe_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
   final ScrollController chatScrollController = ScrollController();
 
-  List<Message> messagesList = [
-    Message(text: 'Hablame!', fromWho: FromWho.me),
-    Message(text: 'Vamos a rodar 🏍?', fromWho: FromWho.me),
-    Message(text: 'Si', fromWho: FromWho.hers),
-  ];
+  List<Message> messagesList = [];
 
   Future<void> sendMessage(String text) async {
     if (text.isEmpty) return;
     final Message newMessage = Message(text: text, fromWho: FromWho.me);
     messagesList.add(newMessage);
+
+    if (text.endsWith('?')) await herReply();
+
     notifyListeners();
-    modeScrollToBottom();
+    moveScrollToBottom();
   }
 
-  Future<void> modeScrollToBottom() async {
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
+    messagesList.add(herMessage);
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> moveScrollToBottom() async {
     await Future.delayed(const Duration(milliseconds: 100));
     chatScrollController.animateTo(
       chatScrollController.position.maxScrollExtent,
