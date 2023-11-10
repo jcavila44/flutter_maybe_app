e26ee12dcc980fe8d7492c29202ba28e8032c526
develop-joseavila <josecarlos.avilaperea@gmail.com>
2023-11-10 08:37:07 -0500

    feat: was refacturated by quicktype.io

--- lib/infrastructure/models/yes_no_model.dart
+++ lib/infrastructure/models/yes_no_model.dart
@@ -1,13 +1,31 @@
+import 'package:flutter_maybe_app/domain/entities/message.dart';
+
 class YesNoModel {
-  String answer;
-  bool forced;
-  String image;
+  final String answer;
+  final bool forced;
+  final String image;
 
-  YesNoModel({required this.answer, required this.forced, required this.image});
+  YesNoModel({
+    required this.answer,
+    required this.forced,
+    required this.image,
+  });
 
-  factory YesNoModel.fromJsonMAp(Map<String, dynamic> json) => YesNoModel(
-        answer: json['answer'],
-        forced: json['forced'],
-        image: json['image'],
+  factory YesNoModel.fromJsonMap(Map<String, dynamic> json) => YesNoModel(
+        answer: json["answer"],
+        forced: json["forced"],
+        image: json["image"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "answer": answer,
+        "forced": forced,
+        "image": image,
+      };
+
+  Message toMessageEntity() => Message(
+        text: answer == 'yes' ? 'Si' : 'No',
+        fromWho: FromWho.hers,
+        imageUrl: image,
       );
 }
